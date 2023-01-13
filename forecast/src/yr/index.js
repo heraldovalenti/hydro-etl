d5e7f98e61124d6c13bddc77f488c1e61ff7bf48
Heraldo Valenti <heraldovalenti@gmail.com>
2023-01-13 09:01:39 -0300

    Catch exception for YR provider

--- forecast/src/yr/index.js
+++ forecast/src/yr/index.js
@@ -3,5 +3,13 @@ import {parseDetailsHtml} from './parse';
 
 export const yrForecast = async (city) => {
   const response = await fetchForecast(city);
-  return parseDetailsHtml(response);
+  try {
+    const result = parseDetailsHtml(response);
+    return result;
+  } catch (e) {
+    console.warn(
+      `Error while parsing results for ${city}: ${JSON.stringify(e)}`,
+    );
+  }
+  return [];
 };
