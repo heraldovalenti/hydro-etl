6162cdee2dc5522e44a73e66fc8c41e2e39e9325
Heraldo Valenti <heraldovalenti@gmail.com>
2023-12-06 09:38:33 -0800

    Added ftp tls configs for self-signed certs

--- one-drive/src/ftp-files/ftpConfig.js
+++ one-drive/src/ftp-files/ftpConfig.js
@@ -1,8 +1,12 @@
 const ftpConfig = {
-  host: process.env["FTP_HOSTNAME"],
-  port: process.env["FTP_PORT"],
-  user: process.env["FTP_USERNAME"],
-  password: process.env["FTP_PASSWORD"],
+  host: process.env['FTP_HOSTNAME'],
+  port: process.env['FTP_PORT'],
+  user: process.env['FTP_USERNAME'],
+  password: process.env['FTP_PASSWORD'],
+  secure: true,
+  secureOptions: {
+    rejectUnauthorized: false,
+  },
 };
 
-module.exports = { ftpConfig };
+module.exports = {ftpConfig};
